79033e290ea990142cd9d76a277b229c9baaf918
Vee Satayamas <vsatayamas@gmail.com>
2015-03-20 21:36:11 +0700

    command line

---
+++ src/cli.rs
@@ -0,0 +1,19 @@
+mod dict;
+mod graph;
+mod acc;
+mod wordcut;
+use std::io;
+use std::io::BufRead;
+use wordcut::Wordcut;
+use dict::Dict;
+
+fn main() {
+    let dict = Dict::load_default();
+    let wordcut = Wordcut::new(dict.unwrap());
+
+    for line in io::BufReader::new(io::stdin()).lines() {
+        let cleaned_line = line.unwrap().trim_right_matches('\n').to_string();
+        let segmented_string = wordcut.put_delimiters(&cleaned_line, "|");
+        println!("{}", segmented_string);
+    }
+}
