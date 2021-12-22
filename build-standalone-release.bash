a23fd7fbcbd6a50a783cb7274b9615d9606de4a4
Vee Satayamas <5ssgdxltv@relay.firefox.com>
2021-12-22 19:56:03 +0100

    build script for Windows

--- build-standalone-release.bash
+++ build-standalone-release.bash
@@ -8,3 +8,13 @@ cp target/release/wordcut $PKG
 cp data/* $PKG
 cp LICE* $PKG
 tar -cvvzf $PKG.tar.gz $PKG
+
+PKGWIN=chamkho-$VERSION-windows-amd64
+docker build -t rustwin -f Dockerfile.windows .
+docker run -it --rm -v $(pwd):/work -w /work -u 1000 rustwin cargo build --release --features=onedir --target=x86_64-pc-windows-gnu
+rm -rf $PKGWIN && mkdir $PKGWIN
+cp target/x86_64-pc-windows-gnu/release/wordcut.exe $PKGWIN
+cp data/* $PKGWIN
+cp LICE* $PKGWIN
+docker build -t zip -f Dockerfile.zip .
+docker run -it --rm -v $(pwd):/work -w /work -u 1000 zip zip -r $PKGWIN.zip $PKGWIN
