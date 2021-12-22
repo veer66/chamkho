e3071c97db3879260454e241be48444cb4d8265e
Vee Satayamas <5ssgdxltv@relay.firefox.com>
2021-12-22 19:03:13 +0100

    Use version from Cargo

--- build-standalone-release.bash
+++ build-standalone-release.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION=1.0.3
-docker run -it --rm -v $(pwd):/work -w /work -u 1000 rust cargo build --release
+VERSION=$(grep ^version Cargo.toml | awk '{ gsub(/"/, "", $3); print $3; }')
+docker run -it --rm -v $(pwd):/work -w /work -u 1000 rust cargo build --release --features=onedir
 PKG=chamkho-$VERSION-linux-amd64
 rm -rf $PKG && mkdir $PKG
 cp target/release/wordcut $PKG
