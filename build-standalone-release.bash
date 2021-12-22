4948cb72f6f084fcd08b542e9e3ad4ec9b321406
Vee Satayamas <5ssgdxltv@relay.firefox.com>
2021-12-22 18:54:43 +0100

    Add license in tar package and put OS name in the package file name

--- build-standalone-release.bash
+++ build-standalone-release.bash
@@ -2,7 +2,9 @@
 
 VERSION=1.0.3
 docker run -it --rm -v $(pwd):/work -w /work -u 1000 rust cargo build --release
-rm -rf chamkho-$VERSION-amd64 && mkdir chamkho-$VERSION-amd64
-cp target/release/wordcut chamkho-$VERSION-amd64
-cp data/* chamkho-$VERSION-amd64
-tar -cvvzf chamkho-$VERSION-amd64.tar.gz chamkho-$VERSION-amd64
+PKG=chamkho-$VERSION-linux-amd64
+rm -rf $PKG && mkdir $PKG
+cp target/release/wordcut $PKG
+cp data/* $PKG
+cp LICE* $PKG
+tar -cvvzf $PKG.tar.gz $PKG
