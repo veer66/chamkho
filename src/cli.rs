8a3ece32ed4fa4f5cfafaba456a969fcdf6b981b
Vee Satayamas <vee@localhost.localdomain>
2022-09-13 22:23:19 +0700

    chore: untabify

--- src/cli.rs
+++ src/cli.rs
@@ -28,14 +28,14 @@ fn main() {
     let dict = lib::load_dict(dict_path).unwrap();
 
     let cluster_rule_path = if let Some(cluster_rules_path) = matches.value_of("cluster_rules_path") {
-	Some(cluster_rules_path.to_string())
+        Some(cluster_rules_path.to_string())
     } else {
-	match lang {
+        match lang {
             Some("lao") => lib::lao_clusters_path(),
             Some("khmer") => lib::khmer_clusters_path(),
             Some("myanmar") => lib::myanmar_clusters_path(),
             Some("thai") | Some(_) | None => lib::thai_cluster_path(),
-	}
+        }
     };
 
     let wordcut = match cluster_rule_path {
