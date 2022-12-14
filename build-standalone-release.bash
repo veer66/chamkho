bbde176b234407bc7aca2e378dde7e9cd415353d
Vee Satayamas <vsatayamas@gmail.com>
2022-12-14 17:43:48 +0700

    feat: add build script for wasm

--- build-standalone-release.bash
+++ build-standalone-release.bash
@@ -1,20 +1,30 @@
 #!/bin/bash
 
 VERSION=$(grep ^version Cargo.toml | awk '{ gsub(/"/, "", $3); print $3; }')
-docker run -it --rm -v $(pwd):/work -w /work -u 1000 rust cargo build --release --features=onedir
-PKG=chamkho-$VERSION-linux-amd64
-rm -rf $PKG && mkdir $PKG
-cp target/release/wordcut $PKG
-cp data/* $PKG
-cp LICE* $PKG
-tar -cvvzf $PKG.tar.gz $PKG
 
-PKGWIN=chamkho-$VERSION-windows-amd64
-docker build -t rustwin -f Dockerfile.windows .
-docker run -it --rm -v $(pwd):/work -w /work -u 1000 rustwin cargo build --release --features=onedir --target=x86_64-pc-windows-gnu
-rm -rf $PKGWIN && mkdir $PKGWIN
-cp target/x86_64-pc-windows-gnu/release/wordcut.exe $PKGWIN
-cp data/* $PKGWIN
-cp LICE* $PKGWIN
-docker build -t zip -f Dockerfile.zip .
-docker run -it --rm -v $(pwd):/work -w /work -u 1000 zip zip -r $PKGWIN.zip $PKGWIN
+function build_pkg () {
+    target=$1
+    name=$2
+    podman run -it --rm -v $(pwd):/work -w /work rust /bin/bash -c "rustup target add $target; cargo build --release --features=onedir --target=$target"
+    PKG=chamkho-$VERSION-$name
+    rm -rf $PKG && mkdir $PKG
+    cp target/$target/release/wordcut $PKG
+    cp data/* $PKG
+    cp LICE* $PKG
+    tar -cvvzf $PKG.tar.gz $PKG
+}
+
+#build_pkg x86_64-unknown-linux-musl linux-x64_64-musl
+build_pkg x86_64-pc-windows-gnu windows-x86_64
+build_pkg wasm32-wasi wasm32-wasi
+# PKGWIN=chamkho-$VERSION-windows-x86_64
+# docker build -t rustwin -f Dockerfile.windows .
+# docker run -it --rm -v $(pwd):/work -w /work -u 1000 rustwin cargo build --release --features=onedir --target=x86_64-pc-windows-gnu
+# rm -rf $PKGWIN && mkdir $PKGWIN
+# cp target/x86_64-pc-windows-gnu/release/wordcut.exe $PKGWIN
+# cp data/* $PKGWIN
+# cp LICE* $PKGWIN
+# docker build -t zip -f Dockerfile.zip .
+# docker run -it --rm -v $(pwd):/work -w /work -u 1000 zip zip -r $PKGWIN.zip $PKGWIN
+
+# wasm32-wasi
